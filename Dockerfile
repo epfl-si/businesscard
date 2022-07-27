83e384e0c63d19a63a170c2f6faa26e315797ebe
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-07-27 17:58:19 +0200

    [feature] Basic Apache configuration
    - Serve on port 8080. You can't serve on port 80 on OpenShift — Don't laugh!
    - Keep the dinfo- and DNS-neutral bits of pieces of 25-businesscard.conf and the old Docker entry point
    - Pass-through the `TEQUILA_FINGERPRINT` environment variable, detected at startup time

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,19 @@
 FROM ghcr.io/epfl-si/common-libs:v0.2.0
 LABEL maintainer "isas-fsd@groupes.epfl.ch"
 
+USER root
+RUN set -e -x; apt -qy update; apt -qy install apache2; apt clean
+RUN a2enmod remoteip
+
+ENV APACHE_RUN_USER www-data
+ENV APACHE_RUN_GROUP root
+ENV APACHE_RUN_DIR /tmp
+ENV APACHE_PID_FILE /tmp/apache.pid
+ENV APACHE_LOG_DIR /tmp
+
+COPY conf/docker/ports.conf /etc/apache2/ports.conf
+
+COPY conf/docker/25-businesscard.conf /etc/apache2/conf-enabled/
+
+COPY conf/docker/docker-entrypoint.sh /
+ENTRYPOINT ["/docker-entrypoint.sh"]
--- conf/docker/docker-entrypoint.sh
+++ conf/docker/docker-entrypoint.sh
@@ -4,12 +4,4 @@
 ###############################################################
 export TEQUILA_FINGERPRINT=`openssl s_client -connect $TEQUILA_HOST:443 < /dev/null 2>/dev/null | openssl x509 -fingerprint -noout -in /dev/stdin | sed "s/ Fingerprint=/$/g" | sed "s/SHA1/sha1/g"`
 
-###############################################################
-# Substitute env vars in config files
-###############################################################
-envsubst < "/home/dinfo/tequila.conf" > "/tmp/tequila.conf"
-cat /tmp/tequila.conf > /etc/tequila.conf
-envsubst < "/home/dinfo/25-businesscard.epfl.ch.conf" > "/tmp/apache.conf"
-cat /tmp/apache.conf > /etc/apache2/sites-available/25-businesscard.epfl.ch.conf
-
-apachectl -e debug -D FOREGROUND
+exec apache2 -e debug -D FOREGROUND
