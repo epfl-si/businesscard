0304081fd67703ed89e2c2a3225d7dae5b213055
Nicolas Borboën <ponsfrilus@gmail.com>
2023-02-09 16:41:47 +0100

    [chore] clean up deploy scripts (checkout businesscard.ops)

--- gettst.sh
+++
@@ -1,5 +0,0 @@
-scp -r dinfo@test-dinfo1:/var/www/vhosts/businesscard.epfl.ch/cgi-bin/\*  cgi-bin/.
-scp -r dinfo@test-dinfo1:/var/www/vhosts/businesscard.epfl.ch/htdocs .
-scp -r dinfo@test-dinfo1:/var/www/vhosts/businesscard.epfl.ch/private/tmpl private/.
-scp -r dinfo@test-dinfo1:/var/www/vhosts/businesscard.epfl.ch/conf .
-
